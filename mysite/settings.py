b90652757e330ac9dac741869db02a0649ff9cca
구세영 <0917sy@gmail.com>
2018-06-14 03:41:15 +0900

    change

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,3 +104,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+
+ENDPOINT = 'https://s3-us-west-2.amazonaws.com/bingbongcloud/media/'
